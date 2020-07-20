7b1359e45b18315eab153ffacba801510df6f8d5
Jane Suh <sourcherryyogurt@gmail.com>
2020-07-19 22:36:26 -0400

    Implemented linear and binary search algorithms

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,23 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
 
-    # Your code here
+    while start <= end:
+        midpoint = (start + end) // 2
 
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] < target:
+            start = midpoint + 1
+        else:
+            end = midpoint - 1
 
     return -1  # not found
